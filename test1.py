6cc8634866e6ae70439fdbfbbdca9f05d8664a5b
didaniel12 <danielek098765432@wp.pl>
2023-09-28 21:54:01 +0200

    update-01

--- test1.py
+++ test1.py
@@ -1,17 +1,6 @@
-def dzielenie(a , b):
-    return a / b
-a = input("wpisz liczbę ")
-b = input(" wpisz liczbę: ")
-try:
-    b = float(b)
-    a = float(a)
-    wynik = a / b
-    a % b
-    if a % b == 0:
-        print(int(wynik))
-    else: print(wynik)
-    
-except ValueError:
-    print("twoja stara")
-except ZeroDivisionError:
-    print("masz downa?")
+name = input("twoje imię: ")
+print("cześć " + name)
+a = input("liczba 1 ")
+b = input("liczba 2 ")
+print(int(a) + int(b))
+print(type("a")) 
